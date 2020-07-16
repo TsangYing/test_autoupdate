381547b15050b0bbdf13b22dd3909aca45dd060a
TsangYing <tsang6961@gmail.com>
2020-07-16 11:45:03 +0800

    change to java

--- app/src/main/java/com/example/test_autoupdate/MainActivity.java
+++ app/src/main/java/com/example/test_autoupdate/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -25,13 +27,24 @@ public class MainActivity extends AppCompatActivity implements  UpdateHelper.OnU
                 .setPositiveButton("UPDATE", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(MainActivity.this, ""+urlApp,Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "open:"+urlApp,Toast.LENGTH_SHORT).show();
+                        /*try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }*/
+                        Intent intent = new Intent();
+                        intent.setAction(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse(urlApp));
+                        startActivity(intent);
+                        System.exit(0);//關閉App
                     }
 
                     }).setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {//Want to that user can select to update?
                         dialogInterface.dismiss();
+                        System.exit(0);
                     }
 
                 }).create();
